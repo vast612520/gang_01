3dccbd05414fbc7a75b729e9c489be1d782bd267
vast612520 <konghuigang_612@foxmail.com>
2020-12-27 10:53:28 +0800

    Initial commit

---
+++ gang_test/src/com/bewind/Gang.java
@@ -0,0 +1,12 @@
+package com.bewind;
+
+/**
+ * Description:
+ * User:Vast_612520
+ * Date:2020/12/27
+ */
+public class Gang {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
